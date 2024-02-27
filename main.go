a68352edb619f3b52b30ea60071c9ad126dc563a
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2024-02-27 18:45:03 +0100

    First Go program :)

---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// "main" is important, go will look for main at the beginning, only one main function in allowed in the app
+func main() {
+	// functions that are exported are with Capital Letter
+	fmt.Println("Hi")
+}
