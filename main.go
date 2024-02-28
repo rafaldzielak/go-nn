28d5564a4d56179b10fbd754c7864fedaa11e24b
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2024-02-28 19:53:45 +0100

    Printing & formatting strings

--- main.go
+++ main.go
@@ -38,4 +38,21 @@ func main() {
 	scoreThree := 1.5 //float64
 	fmt.Println(scoreOne, scoreTwo, scoreThree)
 
+	// Print:
+	fmt.Print("Hi, ")
+	fmt.Print("Rafa \n")
+	fmt.Print("new line \n")
+	fmt.Println("cya")
+
+	// Formatted strings
+	fmt.Printf("my age is %v and my name is %v\n", ageOne, nameFour)
+	fmt.Printf("my age is %q and my name is %q\n", ageOne, nameFour) // with quotes (for strings)
+	fmt.Printf("age is of type %T\n", ageOne)                        // type
+	fmt.Printf("you scored %f points\n", scoreThree)
+	fmt.Printf("you scored %0.2f points\n", scoreThree)
+
+	// Sprintf (save formatted strings)
+	str := fmt.Sprintf("my age is %v and my name is %v\n", ageOne, nameFour)
+	fmt.Println("Saved string: ", str)
+
 }
