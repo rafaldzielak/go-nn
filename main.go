09b0f9ff1b0a201aede5f456b2b787b25e161dc5
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2024-02-29 17:31:23 +0100

    Basic loops

--- main.go
+++ main.go
@@ -96,4 +96,27 @@ func main() {
 	sort.Strings(strs)
 	fmt.Println(strs)
 	fmt.Println(sort.SearchStrings(strs, "dyrek"))
+
+	// LOOPS:
+	x := 0
+	for x < 5 {
+		// fmt.Println("Value of x:", x)
+		x++
+	}
+	for i := 0; i < 5; i++ {
+		// fmt.Println("Value of i:", i)
+	}
+	for i := 0; i < len(strs); i++ {
+		// fmt.Println(strs[i])
+	}
+	// for index, value := range strs {
+	// 	fmt.Printf("the value at index %v is %v\n", index, value)
+	// }
+	for _, value := range strs {
+		fmt.Printf("the value is %v\n", value)
+		value = "new string" // does NOT update the value
+	}
+
+	fmt.Println(strs)
+
 }
